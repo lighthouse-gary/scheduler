07d9843f3ff40ec528d1ebec26e3c6584180bb0d
Gary J <lighthouse@accu.ca>
2023-11-30 21:12:28 -0800

    finished daylist/d2

---
+++ src/components/DayListItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', {
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0,
+  });
+
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">
+        {props.spots === 0
+          ? 'no spots remaining'
+          : `${props.spots} spot${props.spots !== 1 ? 's' : ''} remaining`}
+      </h3>
+    </li>
+  );
+}
